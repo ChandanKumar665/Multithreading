c7fa5a43ee3c1566796a66fcde5ecbd172655d63
Chandan Kumar <ck6652265@gmail.com>
2017-09-07 22:41:54 +0530

    Update PriorityDemoMain.java

--- PriorityDemoMain.java
+++ PriorityDemoMain.java
@@ -1,43 +1,28 @@
-class NewThread extends Thread
-{
-	
-}
-
-class NewThread2 implements Runnable
-{
-	public void run()
-	{
+pacakge com.github.chandan665.multithreading
+class NewThread2 implements Runnable {
+	public void run() {
 		System.out.println("This is NewThread2 class which implementts Rnnable"
 		+"Interface");
 	}
 }
 
-class Priority extends Thread
-{ 
-
-public void run()
-	{
+class Priority extends Thread { 
+public void run() {
 	
-		for(int i=1;i<=5;i++)
-		{
+		for(int i=1;i<=5;i++) {
 			System.out.println("Child Thread");
-		try
-		{
+		try {
 			Thread.sleep(2000);
 		}		
-			catch(InterruptedException e)
-	        {}
+			catch(InterruptedException e) {
+			System.out.println(e.getMessage());
+			}
 		}
 	}	
-	
-	
-
 }
 
-class PriorityDemoMain
-{
-	public static void main(String[]args)
-	{
+class PriorityDemoMain {
+	public static void main(String[]args) {
 		NewThread2 nt=new NewThread2();
 		Thread t3=new Thread(nt);
 		t3.start();
@@ -50,10 +35,8 @@ class PriorityDemoMain
 		t.setDaemon(true);
 		System.out.println(t.isDaemon());
 		t.start();
-		for(int i=0;i<=5;i++)
-		{
+		for(int i=0;i<=5;i++) {
 		System.out.println("main thread");
-		
 		}
 		}
 	}
