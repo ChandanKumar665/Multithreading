8d515fac5e74888c1b49ac8ab5022c773403ec51
Chandan Kumar <ck6652265@gmail.com>
2017-09-07 22:48:14 +0530

    Add files via upload

---
+++ ChildThreadMain.java
@@ -0,0 +1,30 @@
+class ChildThread extends Thread { 
+static Thread mt;
+public void run(){
+	try{
+		mt.join();
+	}
+	catch(InterruptedException e){
+		System.out.println(e.getMessage());
+	}
+	for(int i=0;i<=3;i++) {
+	System.out.println("child Thread");
+	}
+}
+}
+class ChildThreadMain {
+	public static void main(String[]args) {
+		ChildThread.mt=Thread.currentThread();
+		ChildThread t1=new ChildThread();
+		t1.start();
+		for(int i=0;i<=3;i++) {
+			System.out.println("Main thread");
+		try{
+			Thread.sleep(2000);
+			}
+			catch(InterruptedException e){
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
